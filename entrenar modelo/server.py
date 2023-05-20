b258568016933275388572f19131bec1557ebd44
Anwiks <111020665+Anwiks@users.noreply.github.com>
2023-05-20 00:23:07 -0500

    Add files via upload

--- entrenar modelo/server.py
+++ entrenar modelo/server.py
@@ -13,8 +13,8 @@ def set_window_center(window):
     window.geometry('{}x{}+{}+{}'.format(width, height, x, y))
 
 # Función para enviar el resultado al cliente
-def send_result_to_client(result, client_ip, client_port):
-    client_address = (client_ip, client_port)
+def send_result_to_client(result):
+    client_address = ('<172.17.4.111>', 12345)  # Remplace <client_ip> por el ip del cliente
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock.connect(client_address)
         sock.sendall(result.encode())
@@ -23,9 +23,7 @@ def send_result_to_client(result, client_ip, client_port):
 def execute_code():
     code = code_text.get('1.0', tk.END)
     result = subprocess.getoutput(code)
-    client_ip = ip_entry.get()
-    client_port = int(port_entry.get())
-    send_result_to_client(result, client_ip, client_port)
+    send_result_to_client(result)
 
 # Crear la ventana principal
 window = tk.Tk()
@@ -34,18 +32,6 @@ window.title("Enviar Código Ejecutado")
 # Crear un widget de desplazamiento de texto para ingresar el código
 code_text = scrolledtext.ScrolledText(window, width=80, height=20)
 code_text.pack()
-
-# Crear el campo de entrada para la dirección IP del cliente
-ip_label = tk.Label(window, text="Dirección IP del Cliente:")
-ip_label.pack()
-ip_entry = tk.Entry(window)
-ip_entry.pack()
-
-# Crear el campo de entrada para el puerto del cliente
-port_label = tk.Label(window, text="Puerto del Cliente:")
-port_label.pack()
-port_entry = tk.Entry(window)
-port_entry.pack()
 
 # Crear el botón para ejecutar y enviar el código
 execute_button = tk.Button(window, text="Ejecutar y Enviar Código", command=execute_code)
@@ -56,7 +42,6 @@ set_window_center(window)
 
 # Iniciar el bucle principal de la ventana
 window.mainloop()
-
 
 
 
